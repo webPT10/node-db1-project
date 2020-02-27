227265a83fdb316d15167d93d21de3aa026bb437
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-02-26 17:42:35 -0800

    .put and .post CREATED and TESTED

--- server.js
+++ server.js
@@ -27,9 +27,34 @@ server.get("/:id", async (req, res, next) => {
   }
 });
 
-server.post("/", async (req, res, next) => {});
+server.post("/", async (req, res, next) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget
+    };
 
-server.put("/:id", async (req, res, next) => {});
+    const [id] = await db.select("*").from("accounts").insert(payload);
+    const newAccount = await db.select("*").from("accounts").where("id", id).first();
+    res.json(newAccount);
+  } catch (error) {
+    next(error);
+  }
+});
+
+server.put("/:id", async (req, res, next) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget
+    };
+    await db.select("*").from('accounts').where("id", req.params.id).update(payload);
+    const updatedAccount = await db.select("*").from('accounts').where("id", req.params.id);
+    res.json(updatedAccount)
+  } catch (error) {
+    next(error);
+  }
+});
 
 server.delete("/:id", async (req, res, next) => {
   try {
